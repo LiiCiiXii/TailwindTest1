48459eee248524c2f17e0c1d3b6af8005c406930
LiiCiiXii <liiciixii@gmail.com>
2025-02-20 13:13:51 +0700

    V4

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["*.html","./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        myfont1:[
+          "Sigmar", "serif"
+        ],
+        GrechenFuemen:[
+          "Grechen Fuemen", "serif"]
+      },
+      textColor:{
+        pinker:[
+          "#FF91A4"
+        ],
+        reder:[
+          "#FC1012"
+        ]
+      },
+      backgroundColor:{
+        pinker:[
+          "#FF91A4"
+        ]
+      },
+    },
   },
   plugins: [],
 }
